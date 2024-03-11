c09ded89395c6b2f619e3352a4fc571aa18626c6
Julian B <jeriox@users.noreply.github.com>
2024-03-11 16:10:25 +0100

    add signals

--- railml_exporter/exporter.py
+++ railml_exporter/exporter.py
@@ -4,6 +4,7 @@ from xml.etree.ElementTree import ElementTree, Element, SubElement
 
 from yaramo.edge import Edge
 from yaramo.node import Node
+from yaramo.signal import SignalDirection, Signal, SignalKind, SignalFunction
 from yaramo.topology import Topology
 
 
@@ -24,12 +25,12 @@ class Exporter:
         self.level = SubElement(self.network, "level", id="lv01", descriptionLevel="Micro")
         self.funcInfra = SubElement(self.infra, "functionalInfrastructure")
         self.switchesIS = SubElement(self.funcInfra, "switchesIS")
-        #self.signalsIS = SubElement(self.funcInfra, "signalsIS")
+        self.signalsIS = SubElement(self.funcInfra, "signalsIS")
         self.tracks = SubElement(self.funcInfra, "tracks")
         self.interlocking = SubElement(self.root, "interlocking")
         assetsIL = SubElement(self.interlocking, "assetsForInterlockings")
         self.assetsIL = SubElement(assetsIL, "assetsForInterlocking", id="af01")
-        #self.signalsIL = SubElement(self.assetsIL, "signalsIL")
+        self.signalsIL = SubElement(self.assetsIL, "signalsIL")
         self.switchesIL = SubElement(self.assetsIL, "switchesIL")
         self.tree = self.generate_xml()
 
@@ -43,6 +44,19 @@ class Exporter:
         SubElement(relation, "elementB", ref=edge2.uuid)
         SubElement(self.level, "networkResource", ref=relation.get("id"))
         return relation
+
+    def _get_signal_function(self, signal: Signal):
+        if signal.kind == SignalKind.Vorsignal:
+            return "distant"
+        elif signal.kind == SignalKind.Sperrsignal:
+            return "barrage"
+        elif signal.function == SignalFunction.Ausfahr_Signal:
+            return "exit"
+        elif signal.function == SignalFunction.Einfahr_Signal:
+            return "entry"
+        elif signal.function == SignalFunction.Block_Signal:
+            return "block"
+        return None
 
     def generate_xml(self) -> ElementTree:
 
@@ -86,6 +100,16 @@ class Exporter:
                 SubElement(switch_il, "refersTo", ref=node.uuid)
                 SubElement(switch_il, "branchLeft", ref=f"trc_{edge_left.uuid}")
                 SubElement(switch_il, "branchRight", ref=f"trc_{edge_right.uuid}")
+        for yaramo_signal in self.topology.signals.values():
+            signal = SubElement(self.signalsIS, "signalIS", id=yaramo_signal.uuid, isSwitchable="true")
+            SubElement(signal, "isTrainMovementSignal")
+            yaramo_signal.edge.update_length()
+            SubElement(signal, "spotLocation", id=str(uuid4()), netElementRef=yaramo_signal.edge.uuid, pos=str(yaramo_signal.distance_edge / yaramo_signal.edge.length), applicationDirection="normal" if yaramo_signal.direction == SignalDirection.IN else "reverse")
+            if yaramo_signal.name:
+                SubElement(signal, "name", name=yaramo_signal.name, language="en")
+            signal_il = SubElement(self.signalsIL, "signalIL", id=str(uuid4()))
+            if function := self._get_signal_function(yaramo_signal):
+                signal_il.set("function", function)
 
         return ElementTree(self.root)
 
